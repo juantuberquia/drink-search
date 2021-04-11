4256ad88cbd16d40bcbc3b8b25d9332aeb4c9779
juantuberquia <juantuberquia@users.noreply.github.com>
2021-04-11 18:37:39 -0500

    modal ok

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,12 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Recipe = ({ drink }) => {
-  const { setIdDrink } = useContext(ModalContext);
+  const { setIdDrink, saveIdDrink } = useContext(ModalContext);
+
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setIdDrink(null);
+  };
 
   const handleClick = (e) => {
     e.preventDefault();
-
+    handleOpen();
     setIdDrink(drink.idDrink);
   };
 
@@ -27,6 +63,19 @@ const Recipe = ({ drink }) => {
           >
             ver receta
           </button>
+
+          <Modal open={open} onClick={handleClose}>
+            <div style={modalStyle} className={classes.paper}>
+              <h2> {saveIdDrink.strDrink}</h2>
+              <h3 className="mt-4"> Instrucciones </h3>
+              <p> {saveIdDrink.strInstructions}</p>
+              <img
+                className="img-fluid my-4"
+                src={saveIdDrink.strDrinkThumb}
+                alt={saveIdDrink.strDrink}
+              />
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,26 +1,27 @@
 import React, { createContext, useState, useEffect } from "react";
+
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
   const [idDrink, setIdDrink] = useState(null);
+  const [saveIdDrink, setSaveIdDrink] = useState({});
 
   useEffect(() => {
     if (idDrink !== null) {
       const getDataID = async () => {
         let url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
-
         let response = await fetch(url);
         let resJson = await response.json();
-
-        console.log(resJson.drinks);
+        setSaveIdDrink(resJson.drinks[0]);
       };
 
       getDataID();
+      setSaveIdDrink({});
     }
   }, [idDrink]);
 
   return (
-    <ModalContext.Provider value={{ setIdDrink }}>
+    <ModalContext.Provider value={{ setIdDrink, saveIdDrink }}>
       {props.children}
     </ModalContext.Provider>
   );
